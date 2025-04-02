49bcd417490909dda2a6327fc4ccb38182666d10
livebylee <107015550+livebylee@users.noreply.github.com>
2025-04-02 12:46:03 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -21,6 +21,7 @@ bool cmp(medal a, medal b){
 }
 int n,k;
 medal nation[1000];
+int ranking[1000];
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -32,18 +33,18 @@ int main(){
         cin >> nation[i].name >> nation[i].gold >> nation[i].silver >> nation[i].copper;
     }
     sort(nation, nation+n,cmp);
-    int ranking = 1;
-
+    
     for(int i = 0;  i < n; i++){
         medal now = nation[i];
+        ranking[i] = i+1;
         if(i != 0){
             medal prev = nation[i-1];
-            if(prev.gold != now.gold || prev.silver != now.silver || prev.copper != now.copper){
-                ranking++;
+            if(prev.gold == now.gold && prev.silver == now.silver && prev.copper == now.copper){
+                ranking[i] = ranking[i-1];
             }
         }
         if(now.name == k){
-            cout << ranking;
+            cout << ranking[i];
             break;
         }
     }
