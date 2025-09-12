2e35e59ad222529cef10d2036b5edf5f97bfae96
livebylee <107015550+livebylee@users.noreply.github.com>
2025-09-12 23:52:24 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int a,b;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a;
+    cin >> b;
+    cout << a+b;
+}
