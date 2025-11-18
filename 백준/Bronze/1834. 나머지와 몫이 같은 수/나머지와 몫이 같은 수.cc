93290720cd975d2b84fde0368b5907b88dab460b
livebylee <107015550+livebylee@users.noreply.github.com>
2025-11-18 21:33:01 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    long long num;
+    long long result = 0;
+    cin >> num;
+    for (long long i = 1; i < num; i++)
+        result += (num + 1) * i;
+ 
+    cout << result;
+ 
+    return 0;
+}
