523a0c1e1995f61a3a5e8a334ad29bbc6cf8291d
livebylee <107015550+livebylee@users.noreply.github.com>
2025-12-06 23:45:00 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2308 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0);
+    int N;
+    string pattern, input;
+    cin >> N;
+    cin >> pattern;
+    pattern.replace(pattern.find("*"), 1, "\\w*");
+    regex re(pattern, regex::optimize);
+    while (N--)
+    {
+        cin >> input;
+        if (regex_match(input, re))
+            cout << "DA";
+        else
+            cout << "NE";
+        cout << "\n";
+    }
+    return 0;
+}
